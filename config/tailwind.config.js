af491067ce6af150ba8fd5c43016b4b0a70c2bb4
Prometheus <berjistechnologies@gmail.com>
2023-07-23 17:55:02 +0300

    Command Center template

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,72 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    mode: 'jit',
+    content: [
+        './public/*.html',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/views/**/*.{erb,haml,html,slim}'
+    ],
+    darkMode: 'class',
+    theme: {
+        fontFamily: {
+            display: ['Open Sans', 'sans-serif'],
+            body: ['Open Sans', 'sans-serif'],
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                14: '14px',
+            },
+            backgroundColor: {
+                'main-bg': '#FAFBFB',
+                'main-dark-bg': '#20232A',
+                'secondary-dark-bg': '#33373E',
+                'light-gray': '#F7F7F7',
+                'half-transparent': 'rgba(0, 0, 0, 0.5)',
+            },
+            borderWidth: {
+                1: '1px',
+            },
+            borderColor: {
+                color: 'rgba(0, 0, 0, 0.1)',
+            },
+            width: {
+                400: '400px',
+                760: '760px',
+                780: '780px',
+                800: '800px',
+                1000: '1000px',
+                1200: '1200px',
+                1400: '1400px',
+            },
+            height: {
+                80: '80px',
+            },
+            minHeight: {
+                590: '590px',
+            },
+            backgroundImage: {
+                'hero-pattern':
+                    "url('https://theexpatmummy.com/wp-content/uploads/2022/12/IMG_9594-1-scaled.jpeg')",
+            },
+        },
+        screens: {
+            xs: '320px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        }
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/container-queries'),
+    ]
+};
